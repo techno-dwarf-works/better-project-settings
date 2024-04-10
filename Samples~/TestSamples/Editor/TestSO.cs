8efb8bf1afb92e635b74121db41f0ba92e7cf3f4
uurha <uurha@users.noreply.github.com>
2024-04-10 14:30:21 +0000

    Merge pull request #1 from techno-dwarf-works/feature/refactoring
    Refactoring

--- Samples~/TestSamples/Editor/TestSO.cs
+++ Samples~/TestSamples/Editor/TestSO.cs
@@ -1,11 +1,13 @@
 ﻿using Better.ProjectSettings.Runtime;
 using Better.Singletons.Runtime.Attributes;
 using UnityEditor;
+using UnityEngine;
 
 namespace Test
 {
     [ScriptableCreate(nameof(Better) + "/" + nameof(Editor) + "/" + nameof(Test), "Test1")]
     public class TestSO : ScriptableSettings<TestSO>
     {
+        [Header("Test")] [SerializeField] private bool _testBool;
     }
 }
